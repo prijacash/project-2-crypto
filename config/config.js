fd2a8909897f9317ea6899850fb8b5c4cc5f6f66
prijacash <prijacash@gmail.com>
2022-09-19 14:45:57 -0700

    prep repo for deploy

---
+++ config/config.js
@@ -0,0 +1,18 @@
+
+// in ./config/config.js
+const fs = require('fs')
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    // add the key/values pairs from your config.json here
+        "development": {
+          "database": "express_auth_boilerplate",
+          "host": "127.0.0.1",
+          "dialect": "postgres"
+        }
+  },
+  production: {
+    use_env_variable: 'DATABASE_URI'
+  }
+}
